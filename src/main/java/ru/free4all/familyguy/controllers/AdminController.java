0d452450c8db69e29d41c64d72dec5862012a3b0
alittlem1nd <evgeny.hodz@gmail.com>
2018-12-21 21:55:24 +0300

    2: fixed duplicates done, but need to reorganize admin panel

--- src/main/java/ru/free4all/familyguy/controllers/AdminController.java
+++ src/main/java/ru/free4all/familyguy/controllers/AdminController.java
@@ -6,44 +6,27 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import ru.free4all.familyguy.entities.Video;
 import ru.free4all.familyguy.repos.VideoRepo;
 import ru.free4all.familyguy.service.AdminService;
-
-import java.util.List;
 
 @Controller
 public class AdminController {
 
-    @Autowired
-    private AdminService adminService;
+    private final AdminService adminService;
+
+    private final VideoRepo videoRepo;
 
     @Autowired
-    private VideoRepo videoRepo;
+    public AdminController(AdminService adminService, VideoRepo videoRepo) {
+        this.adminService = adminService;
+        this.videoRepo = videoRepo;
+    }
 
     @GetMapping("/admin")
     public String admin(Model m) {
-        check();
         m.addAttribute("list", videoRepo.findAll());
         return "admin";
     }
-
-    /**
-     * To delete after first use :(
-     */
-    private void check(){
-        String wrong = "height=\"100%";
-        String correct = "height=\"100%\"";
-        List<Video> videoList = videoRepo.findAll();
-        for(Video v : videoList) {
-            if(!v.getLink().contains(correct) && v.getLink().contains(wrong)) {
-                String replaced = v.getLink().replace(wrong, correct);
-                v.setLink(replaced);
-                videoRepo.save(v);
-            }
-        }
-    }
-
 
     @GetMapping("/login")
     public String login() {
